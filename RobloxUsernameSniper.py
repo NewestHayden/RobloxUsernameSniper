6068966c134ca38ce2cb4aeaabea111df809c85a
NewestHayden <46233564+NewestHayden@users.noreply.github.com>
2020-09-09 13:07:49 -0500

    Updated to add a archive of checked usernames.

--- RobloxUsernameSniper.py
+++ RobloxUsernameSniper.py
@@ -2,22 +2,29 @@ import robloxpy
 import requests
 import time
 
-i = 1500
+i = 15700
 maxval2 = 9999999999999999999
-print("Haydens Number Username Sniper")
+print("Haydens Number Username Sniper\nBeing saved to UserArchive.txt under your user file")
 
 while True:
-     if i == maxval2:
+    file1 = open("UserArchive.txt", 'a')
+    if i == maxval2:
             print("No number usernames open!")
+            file1.write("\nAll number usernames taken (wait what?)")
+            file1.close()
             break
-     else:
+    else:
         iuse = str(i)
         result = robloxpy.DoesNameExist(iuse)
         if result == False:
             print("Username " + iuse + " is open")
+            file1.write(iuse + " is open!")
+            file1.close()
             break
         else:
              print(iuse + " is taken")
+             file1.write("\n" +iuse + " is in use")
+             file1.close()
              i = i +1
              time.sleep(1)
             
